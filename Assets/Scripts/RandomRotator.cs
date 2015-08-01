241308867f2154ad56401c290e4246537ff751f4
aytona <christopher.aytona@gmail.com>
2015-07-31 20:14:05 -0400

    Creating Hazards

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+
+    Rigidbody rb;
+    public float tumble = 3f;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        rb.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+}
